38bc2ec44fea0f447dfb0241ab7029ee852a2a14
Miguel Ángel Durán <miduga@gmail.com>
2023-12-28 20:53:16 +0100

    Deploy to Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,5 +12,7 @@ export default defineConfig({
     routing: {
       prefixDefaultLocale: false
     }
-  }
+  },
+  output: "server",
+  adapter: vercel()
 });
