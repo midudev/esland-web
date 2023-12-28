eb9c5e8e9e782fe4c837e54f98377ce843504cb9
Miguel Ángel Durán <miduga@gmail.com>
2023-12-28 20:03:32 +0100

    Add i18n and catalan. Viva Andorra! 🇦🇩

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,12 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  i18n: {
+    defaultLocale: 'es',
+    locales: ['es', 'ca'],
+    routing: {
+      prefixDefaultLocale: false
+    }
+  }
 });
