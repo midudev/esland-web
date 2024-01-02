fb4117caf13b6398b3477c9e10524cefc16a1dd6
Miguel Ángel Durán <miduga@gmail.com>
2024-01-02 10:46:19 +0100

    Avoid CLS

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -2,7 +2,10 @@
 module.exports = {
   // i am just using the standard config, change if you need something else
   ...require('prettier-config-standard'),
-  plugins: ['prettier-plugin-astro', "prettier-plugin-tailwindcss"],
+  plugins: [
+    'prettier-plugin-astro',
+    'prettier-plugin-tailwindcss'
+  ],
   overrides: [
     {
       files: '*.astro',
