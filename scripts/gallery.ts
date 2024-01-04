635b1ac33f1033cd5d5c8d0e674716dff4758ef3
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2024-01-04 23:52:22 +0100

    Generate thumbnails from jpg files

--- scripts/gallery.ts
+++ scripts/gallery.ts
@@ -1,26 +1,40 @@
 import { Glob } from 'bun';
 import sharp from 'sharp';
 import { join, dirname, extname, basename } from 'node:path';
-import { rm } from 'node:fs/promises';
 
 const glob = new Glob('public/**/gallery/*.jpg');
 
 const shouldRemove = (str: string = '') => str.toLowerCase().startsWith('rm');
 const remove = shouldRemove(process.argv[2]?.toLowerCase());
 
+const publicPath = join(process.cwd(), 'node_modules', '@imgly/background-removal-node', 'dist');
+const WIDTH = 365;
+const HEIGHT = 365;
+
 for await (const file of glob.scan('.')) {
-  console.info(`Converting ${file}`);
+  console.info(`Removing background of image ${file}`)
+  console.info(`Converting ${file} to webp`);
   const ext = extname(file);
   const fileWithoutExt = basename(file, ext);
   const dir = dirname(file);
-  const webpFile = join(dir, fileWithoutExt) + '1-1.webp';
-  const convert = sharp(file)
-    .trim({ threshold: 0 }) // This removes transparent pixels
+  const filePathWithoutExt = join(dir, fileWithoutExt);
+  const webpFile = filePathWithoutExt + '.webp'; // original as webp
+  const thumbnailFile = filePathWithoutExt + '-thumb.webp';
+  const convertOriginal = sharp(file);
+
+  const originalDetails = await convertOriginal
     .webp({
       lossless: true,
       quality: 100
-    });
+    })
+    .toFile(webpFile);
+  console.info(`Converted to ${webpFile}`, { details: originalDetails });
 
-  await convert.toFile(webpFile);
-  console.info(`Converted to ${webpFile}`);
+  const thumbnail = await convertOriginal
+    .resize(WIDTH, HEIGHT, {
+      fit: 'cover',
+      position: sharp.strategy.entropy,
+    }).toFile(thumbnailFile);
+  console.info(`Created thumbnail ${thumbnailFile}`, { details: thumbnail });
+
 }
