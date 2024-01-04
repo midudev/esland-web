ed73a7494f0f99e4ccb250cefff2dead7fb7ec49
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2024-01-04 22:16:12 +0100

    Convert all images in the project to webp

---
+++ scripts/convert-all.ts
@@ -0,0 +1,33 @@
+import { Glob } from 'bun';
+import sharp from 'sharp';
+import { join, dirname, extname, basename } from 'node:path';
+import { rm } from 'node:fs/promises';
+
+const glob = new Glob('public/**/*.{jpg,jpeg,png}');
+
+const shouldRemove = (str: string) => str === "remove" || str === "true" || str === "1" || str === "on";
+const remove = shouldRemove(process.argv[2]);
+
+console.log(process.argv, { remove })
+
+for await (const file of glob.scan('.')) {
+  console.info(`Converting ${file}`);
+  const ext = extname(file);
+  const newFileName = basename(file, ext);
+  const dir = dirname(file);
+  const newFilePath = join(dir, newFileName) + '.webp';
+  const convert = sharp(file)
+    .trim({ threshold: 0 })
+    .webp({
+      lossless: true,
+      quality: 100
+    });
+
+  await convert.toFile(newFilePath);
+  console.info(`Converted to ${newFilePath}`);
+
+  if (remove) {
+    console.log(`Removing old file ${file}`)
+    await rm(file, { force: true })
+  }
+}
