e8cfd6c633d86796b1d0ac864a9e1c8043da102d
Miguel Ángel Durán <miduga@gmail.com>
2024-01-08 12:47:41 +0100

    Add astro auth

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
-
 import preact from "@astrojs/preact";
+
+import auth from "auth-astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact()],
+  integrations: [tailwind(), preact(), auth()],
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'ca'],
