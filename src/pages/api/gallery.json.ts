70f9950baab3861f48191d53a40b4b97ae879cf9
Miguel Ángel Durán <miduga@gmail.com>
2024-01-10 11:51:07 +0100

    Add API route for gallery.json

---
+++ src/pages/api/gallery.json.ts
@@ -0,0 +1,17 @@
+import type { APIRoute } from "astro";
+
+import galleryInfo from '@/data/meta-gallery.json'
+
+export const GET: APIRoute = ({ params, request }) => {
+  const { url } = request
+  const searchParams = new URL(url).searchParams
+  
+  const edition = Number(searchParams.get('edition') ?? '1')
+  const offset = Number(searchParams.get('offset') ?? '0')
+
+  const editionIndex = edition - 1
+  const editionInfo = galleryInfo[editionIndex]
+
+  return new Response(JSON.stringify(editionInfo.slice(offset))
+  )
+}
