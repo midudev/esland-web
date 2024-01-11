03f5dee0a25680ea429d851f9a4ef45eab0f40c0
Miguel Ángel Durán <miduga@gmail.com>
2024-01-11 20:01:04 +0100

    Add simple vote system

--- src/components/VoteSystem.tsx
+++ src/components/VoteSystem.tsx
@@ -11,11 +11,15 @@ interface Candidate {
   enlace?: string
 }
 
+type Votes = Array<Array<number>>
+
 const MAX_CATEGORIES = 12
+const MAX_VOTES_PER_CATEGORY = 4
 
 export function VoteSystem () {
   const [pageInfo, setPageInfo] = useState<PageInfo>()
   const [category, setCategory] = useState(0)
+  const [votes, setVotes] = useState<Votes>(Array.from({ length: MAX_CATEGORIES }, () => []))
 
   useEffect(() => {
     async function fetchCandidates () {
@@ -33,7 +37,29 @@ export function VoteSystem () {
     setCategory(categoryIndex)
   }
 
+  const handleVote = (
+    { category, candidate }:
+    { category: number, candidate: number }
+  ) => {
+    const votesCategory = votes[category]
+
+    // if it was already voted the item, remove it
+    if (votesCategory.includes(candidate)) {
+      const newVotes = votesCategory.filter((vote) => vote !== candidate)
+      setVotes(prevVotes => prevVotes.with(category, newVotes))
+      return
+    }
+
+    // check if the user has already voted for this category 4 times
+    if (votesCategory.length >= 4) return
+
+    // otherwise, add the vote
+    const newVotes = [...votesCategory, candidate]
+    setVotes(prevVotes => prevVotes.with(category, newVotes))
+  }
+
   const { categoria = '', candidatos } = pageInfo ?? {}
+  const votesCategory = votes[category]
 
   return (
     <div class="mx-auto flex flex-col max-w-7xl pt-40">
@@ -43,18 +69,24 @@ export function VoteSystem () {
 
       <ul class="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-5 gap-2 px-2 xl:px-0">
         {
-          candidatos?.map((candidate) => {
+          candidatos?.map((candidate, index) => {
+            const isVoted = votesCategory.includes(candidatos.indexOf(candidate))
             return (
-              <li>
+              <li class={`${isVoted ? 'bg-yellow-500' : 'bg-blue-900 hover:bg-sky-500'} transition p-1 text-center`} >
+                <button onClick={() => handleVote({ category, candidate: index })}>
                 <img src={`/voting-assets/${candidate.imagen}`} alt={candidate.nombre} />
                 <p>{candidate.nombre}</p>
+                </button>
               </li>
             )
           })
         }
       </ul>
 
-      <footer class="flex justify-center items-center mt-4">
+      <footer class="flex justify-center items-center mt-4 gap-x-20">
+        <div class="flex justify-center items-center gap-x-4 bg-black/50 backdrop-blur-lg px-2 rounded py-3">
+          Votos realizados {votesCategory.length}/{MAX_VOTES_PER_CATEGORY}
+        </div>
         <div class="flex justify-center items-center gap-x-4 bg-black/50 backdrop-blur-lg px-2 rounded py-2">
         <button class="rounded border border-white hover:border-transparent hover:bg-white hover:text-sky-800 p-2 transition" onClick={() => handleNavigation(category - 1)}>
           <Arrow rotated />
