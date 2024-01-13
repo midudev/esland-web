855f98c245274030085fb08d68941ceb9444df23
Miguel Ángel Durán <miduga@gmail.com>
2024-01-13 16:20:20 +0100

    Remove not needed parameter

--- src/pages/api/gallery.json.ts
+++ src/pages/api/gallery.json.ts
@@ -2,7 +2,7 @@ import type { APIRoute } from "astro";
 
 import galleryInfo from '@/data/meta-gallery.json'
 
-export const GET: APIRoute = ({ params, request }) => {
+export const GET: APIRoute = ({ request }) => {
   const { url } = request
   const searchParams = new URL(url).searchParams
   
