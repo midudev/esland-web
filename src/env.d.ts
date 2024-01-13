834b78a5d36ee61aad7e19a8fdde2fe4f96b9da1
Loduis Madariaga <loduis@gmail.com>
2024-01-13 17:11:49 -0500

    refactor: Employing Astro.locals and middleware for i18n content loading.

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,8 @@
+/// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+declare namespace App {
+    interface Locals {
+        i18n: CollectionEntry<'i18n'>
+    }
+}
