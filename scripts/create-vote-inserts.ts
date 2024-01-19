64be9a4be832f80a80591b479e15b93aa6f84202
Whiskydog <alan.varde@gmail.com>
2024-01-19 12:53:48 -0300

    Add script for SQL INSERT statements for voting categories

---
+++ scripts/create-vote-inserts.ts
@@ -0,0 +1,30 @@
+import { join } from 'node:path'
+
+const editionsVotesFilePath = process.argv[2]
+if (editionsVotesFilePath === undefined) {
+  console.error('Must provide editions file path')
+  process.exit(1)
+}
+
+const editionsVotesFile = Bun.file(editionsVotesFilePath)
+const votes: [{
+  categoria: string,
+  candidatos: [{ nombre: string, imagen: string, enlace: string, id: string }],
+  id: string
+}] = JSON.parse(await editionsVotesFile.text())
+const inserts: string[] = []
+
+votes.forEach(({ categoria: categoryName, candidatos: categoryCandidates, id: categoryId }) => {
+  inserts.push(
+    `INSERT INTO Categories (category_id, category_name) VALUES ("${categoryId}", "${categoryName}")`
+  )
+  categoryCandidates.forEach((candidate) => {
+    const { id: candidateId, nombre: candidateName } = candidate
+    inserts.push(
+      `INSERT INTO Options (option_id, category_id, option_name) VALUES ("${candidateId}", "${categoryId}", "${candidateName}")`
+    )
+  })
+})
+
+const outputPath = join(process.cwd(), 'src/data/vote-inserts.sql')
+await Bun.write(outputPath, inserts.join('\n'))
