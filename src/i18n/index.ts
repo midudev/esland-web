78e2cb22a1764cf34167e1b7b0c9535171f72011
Cristian Rivas <cristianriv@gmail.com>
2024-01-21 18:18:31 -0500

    Add language option to i18n

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,8 +1,10 @@
 import catalan from '@/i18n/ca.json'
+import english from '@/i18n/en.json'
 import spanish from '@/i18n/es.json'
 
 const LANGUAGES = {
   CATALAN: 'ca',
+  ENGLISH: 'en',
   SPANISH: 'es'
 }
 
@@ -12,6 +14,7 @@ export const getI18N = ({
   currentLocale: string | undefined
 }) => {
   if (currentLocale === LANGUAGES.CATALAN) return catalan
+  if (currentLocale === LANGUAGES.ENGLISH) return english
   if (currentLocale === LANGUAGES.SPANISH) return spanish
   return spanish
 }
