43caa9ac3622a4fc760e01774e2f01258ccda0b8
Loduis Madariaga <loduis@gmail.com>
2024-02-06 05:08:33 -0500

    refactor: extract styles.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,34 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
-import preact from "@astrojs/preact";
-import i18n from "./src/i18n/config";
-import svelte from '@astrojs/svelte';
-import auth from "auth-astro";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel/serverless'
+import preact from '@astrojs/preact'
+import i18n from './src/i18n/config'
+import svelte from '@astrojs/svelte'
+import auth from 'auth-astro'
 
-import vue from "@astrojs/vue";
+import vue from '@astrojs/vue'
 // https://astro.build/config
 
 export default defineConfig({
   integrations: [
-    tailwind(),
+    tailwind({
+      applyBaseStyles: false
+    }),
     preact(),
     auth(),
     vue(),
     svelte()
   ],
   i18n,
-  trailingSlash: "never",
-  output: "server",
+  trailingSlash: 'never',
+  output: 'server',
   adapter: vercel(),
   build: {
     assets: 'assets'
   },
-  redirects: i18n.routing.prefixDefaultLocale ? {
-    '/': `/${i18n.defaultLocale}`
-  } : undefined
-});
+  redirects: i18n.routing.prefixDefaultLocale
+    ? {
+        '/': `/${i18n.defaultLocale}`
+      }
+    : undefined
+})
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,27 +1,28 @@
-import animations from "tailwindcss-animated";
+import animations from 'tailwindcss-animated'
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
       fontFamily: {
-        tomaso: ["Tomaso", "sans-serif"],
-        jura: ["Jura", "sans-serif"],
+        sans: '"Onest Variable", system-ui, sans-serif',
+        tomaso: ['Tomaso', 'sans-serif'],
+        jura: ['Jura', 'sans-serif']
       },
       colors: {
-        primary: "rgb(253 224 71);",
-      },
-    },
+        primary: 'rgb(253 224 71);'
+      }
+    }
   },
   plugins: [
     animations,
     ({ addComponents }) => {
       addComponents({
-        ".cp-v": {
-          clipPath: "polygon(0 0, 100% 0, 100% 85%, 50% 100%, 50% 100%, 0 85%)",
-        },
-      });
-    },
-  ],
-};
+        '.cp-v': {
+          clipPath: 'polygon(0 0, 100% 0, 100% 85%, 50% 100%, 50% 100%, 0 85%)'
+        }
+      })
+    }
+  ]
+}
