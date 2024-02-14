2d60c85508c2ce61e7b0e7f9c98082653be3cadf
Shadowh4k <rafaeldemoyamendez@gmail.com>
2024-02-14 04:42:49 -0400

    Fix: duplicated masonry-layout in galeria.tsx

--- src/components/Galeria.tsx
+++ src/components/Galeria.tsx
@@ -56,43 +56,6 @@ export default function Galeria({i18n,edicion}:{i18n:any,edicion:string}) {
           ))
         }
       </masonry-layout>
-      <masonry-layout
-        gap="24"
-        maxcolwidth="600"
-        class="lg:mx-auto mx-4 py-20"
-        id="gallery"
-      >
-        {
-          photos.map(({ height, width }, i) => (
-            <a
-              class="group rounded-xl hover:scale-105 hover:contrast-[110%] transition-all relative"
-              href={`/archivo-page/${edicion}/gallery/img-${i + 1}.webp`}
-              target="_blank"
-              data-cropped="true"
-              data-pswp-width={width}
-              data-pswp-height={height}
-              ref={!first.current ? first : undefined}  
-            >
-              <img
-                class="rounded-xl object-cover w-full h-auto"
-                loading="lazy"
-                src={`/archivo-page/${edicion}/gallery/thumbnails/img-${
-                  i + 1
-                }.webp`}
-                alt="Fotografía de los premios ESLAND"
-              />
-              <img
-                class="blur-md opacity-0 group-hover:opacity-100 absolute inset-0 transition contrast-150 -z-10 object-cover"
-                loading="lazy"
-                src={`/archivo-page/${edicion}/gallery/thumbnails/img-${
-                  i + 1
-                }.webp`}
-                alt="Imagen con efecto blur para hacer de sombra de una fotografía de los premios ESLAND"
-              />
-            </a>
-          ))
-        }
-      </masonry-layout>
 
       <div class="text-center mx-auto">
       {
